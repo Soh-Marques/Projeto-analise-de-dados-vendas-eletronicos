0a0dcbe27e9948eaed94334e133316100816668c
Solange Marques <94070435+Soh-Marques@users.noreply.github.com>
2025-10-30 16:28:56 -0300

    Update Consulta_01.sql

--- Projeto SQL/consultas/Consulta_01.sql
+++ Projeto SQL/consultas/Consulta_01.sql
@@ -3,8 +3,13 @@ SELECT
 --strtime é a função usada para formatar data ou 
 --hora dependendo do que for pedido
     STRFTIME('%m', DataVenda) AS Mes,
+       /*sum é função de somatoria, neste caso 
+    ele vai somar o total de faturamento de cada mes*/
     SUM(Total) AS FaturamentoTotal,
+     /* ROUND tem função de arredondamento, 
+    AVG calcula a media do total */
     ROUND(AVG(Total), 2) AS TicketMedio
 FROM Vendas
 GROUP BY Mes
 ORDER BY Mes;
+
