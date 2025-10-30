d3778d2fac0259e2592d9688a90306ca9fe7a755
Solange Marques <94070435+Soh-Marques@users.noreply.github.com>
2025-10-30 16:27:35 -0300

    Update Consulta_02.sql

--- Projeto SQL/consultas/Consulta_02.sql
+++ Projeto SQL/consultas/Consulta_02.sql
@@ -1,6 +1,12 @@
 --Ranking de vendedores por faturamento
 SELECT 
+    /* Dica Legal
+Sempre que vir algo como "X.Coluna", lembre-se:
+"X" é o apelido (ou a própria tabela);
+"Coluna" precisa existir dentro dessa tabela.
+ */
     V.Nome AS Vendedor,
+    
     SUM(Ve.Total) AS TotalVendas,
     COUNT(Ve.VendaID) AS QtdeVendas,
     ROUND(AVG(Ve.Total), 2) AS TicketMedio
@@ -8,3 +14,4 @@ FROM Vendedores V
 JOIN Vendas Ve ON V.VendedorID = Ve.VendedorID
 GROUP BY V.Nome
 ORDER BY TotalVendas DESC;
+
